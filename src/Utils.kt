6cd0569eeb41e353f170205dccc15cc222acb503
Nutriz <jerome.gully0@gmail.com>
2021-12-05 16:08:36 +0100

    Day 1

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,8 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 /**
  * Reads lines from the given input txt file.
@@ -11,3 +13,23 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+@OptIn(ExperimentalTime::class)
+inline fun <T> measureAndLog(textToLog: String = "measured", block: () -> T): T {
+    val timed = measureTimedValue(block)
+    println("$textToLog ${timed.duration}")
+    return timed.value
+}
+
+fun check(part: String, result: Int, excepted: Int) {
+    val sign = if (result == excepted) "==" else "!="
+    val emoji = if (result == excepted) "✅" else "⚠️"
+    println("check $part: $result $sign $excepted $emoji")
+}
+
+/**
+ * Extension functions
+ */
+fun String.binToInt(): Int {
+    return Integer.parseInt(this,2)
+}
